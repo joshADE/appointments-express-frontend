97f0aae6759751d62b9cf877cd597a60c544529b
Josh A <joshmail9510@gmail.com>
2021-08-08 22:56:37 -0400

    Style changes

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import ProfileNav from '../pages/auth/ProfileNav';
 import { SidebarData } from './SidebarData'
-
+import * as HiIcons from 'react-icons/hi'
 const isActive = (path: string, match: any, location: any) => !!(match || path === location.pathname);
 
 class Sidebar extends React.Component {
@@ -10,13 +10,13 @@ class Sidebar extends React.Component {
     render(){
         return (
             <nav 
-                className="w-24 lg:hover:w-48 group h-full flex flex-col items-center justify-around p-0 z-10 transition-all duration-300 ease-linear border-r border-gray-400"
+                className="w-24 lg:hover:w-48 group h-full flex flex-col items-center justify-around p-0 z-10 transition-all duration-300 ease-linear border-4 border-gray-300"
             >
 
                 <Link 
                     to="/"
-                    className="font-mono text-center text-green-900 rounded-lg border-2 border-green-900 w-8 hover:bg-green-100"
-                >AE</Link>
+                    className="text-center w-12"
+                ><HiIcons.HiOutlineMenuAlt2 className="w-6 h-6 mx-auto" /></Link>
 
                 <ul
                     className="flex flex-col items-center justify-center m-5"
@@ -25,7 +25,7 @@ class Sidebar extends React.Component {
                         return (
                             <li 
                                 key={index} 
-                                className=" flex items-center justify-center rounded-lg p-2 mb-6 w-3/4 bg-white text-black"
+                                className=" flex items-center justify-center rounded-lg p-2 mb-6 w-3/4 bg-white text-gray-600 hover:text-black"
                             >
                                 <NavLink exact title={item.title} className="flex" to={item.path} activeClassName="text-green-800" isActive={isActive.bind(this,item.path)}>
                                     <span
