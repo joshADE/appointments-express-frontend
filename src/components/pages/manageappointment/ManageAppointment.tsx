5f461f3355d034866e9c0cd138259e4e6d87c977
Josh A <joshmail9510@gmail.com>
2021-08-21 22:00:34 -0400

    Style fixes

--- src/components/pages/manageappointment/ManageAppointment.tsx
+++ src/components/pages/manageappointment/ManageAppointment.tsx
@@ -4,14 +4,12 @@ import DashboardPageHeader from '../../shared/DashboardPageHeader';
 
 const ManageAppointment: React.FC = () => {
         return (
-        <div className="overflow-y-auto h-full w-11/12 font-roboto p-4 grid gap-4 grid-cols-1 md:grid-cols-4">
-            <div className="md:col-span-3">
-                <DashboardPageHeader
-                    title="Appointments"
-                    description="Here are all you scheduled appointments. You can see more details about the event by clicking. Make sure to select a store first."
-                />
-            </div>
-            <div className="md:col-span-1">
+        <div className="overflow-y-auto h-full w-11/12 font-roboto p-4">
+            <DashboardPageHeader
+                title="Appointments"
+                description="Here are all you scheduled appointments. You can see more details about the event by clicking. Make sure to select a store first."
+            />
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-4">
                 
             </div>
         </div>
